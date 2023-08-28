5ec1cff47473e169dfabc779163bef2e8dd7e5dc
Georgy Baranov <floridavbv@gmail.com>
2023-08-27 22:53:11 -0600

    migrations

--- Models/Domain/Category.cs
+++ Models/Domain/Category.cs
@@ -3,9 +3,11 @@ namespace net.Domain
 {
 	public class Category
 	{
-		public Category()
-		{
-		}
-	}
+		public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string UrlHandle { get; set; }
+    }
 }
 
