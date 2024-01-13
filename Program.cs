ef9979de71ad19eee3e154143d2df461c5ae2d31
Georgy Baranov <floridavbv@gmail.com>
2024-01-12 21:56:43 -0700

    test release

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using net.Repositories.Implementation;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
-// Test change
+// Test change, another change
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
